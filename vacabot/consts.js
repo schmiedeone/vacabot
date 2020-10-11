5aca52cdb07bf174278523d5727b696e043a065a
Mukku <mukarram.ali89@gmail.com>
2020-10-11 14:18:44 +0200

    [CLEANUP] removed unused const

--- vacabot/consts.js
+++ vacabot/consts.js
@@ -5,7 +5,6 @@ module.exports = Object.freeze({
   SUBMIT_VACATION_DIALOG: 'SUBMIT_VACATION_DIALOG',
   DENY_VACATION_REQUEST: 'DENY_VACATION_REQUEST',
   DEFAULT_VACATION_BALANCE: 10,
-  DEFAULT_USER: { userName: 'mukarram.ali89', userId: 'U01C90R2NDB', teamId: 'T01CQMVB997' },
   AUTH_TOKEN: process.env.BOT_USER_AUTH_TOKEN,
   MODAL_OPEN_URL: 'https://slack.com/api/views.open',
   POST_MSG_URL: 'https://slack.com/api/chat.postMessage',
